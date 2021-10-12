5251ea2842d465f18342976b079178328c847538
Justin Fu <JFu@Lcs.on.ca>
2021-10-12 10:01:45 -0400

    T-shirt drawing, almost done. Only have 4 angles on the corner and 2 side shape left.

--- Playground.playground/Pages/Empty Canvas 2.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Empty Canvas 2.xcplaygroundpage/Contents.swift
@@ -58,33 +58,113 @@ canvas.drawAxes(withScale: true, by: 20, color: .black)
 
 // Begin writing your code below (you can remove the examples shown)
 
-// Draw a circle, using the canvas object directly
-canvas.drawEllipse(at: Point(x: 100, y: 100), width: 25, height: 25)
+// back to origin
+func goToOrigin() {
+    p.goto(dx: Double(p.currentPosition().x) * -1.0, dy: Double(p.currentPosition().y) * -1.0)}
+    
+//draw centre lines
+p.drawTo(dx: 100, dy: 100)
+goToOrigin()
+p.drawTo(dx: 100, dy: -100)
+goToOrigin()
+p.drawTo(dx: -100, dy: 100)
+goToOrigin()
+p.drawTo(dx: -100, dy: -100)
+goToOrigin()
+p.drawTo(dx: 40, dy: 100)
+goToOrigin()
+p.drawTo(dx: 100, dy: 60)
+goToOrigin()
+p.drawTo(dx: -40, dy: -100)
+goToOrigin()
+p.drawTo(dx: -100, dy: -60)
+goToOrigin()
+p.drawTo(dx: -100, dy: 60)
+p.drawTo(dx: 200, dy: -120)
+goToOrigin()
+p.drawTo(dx: -40, dy: 100)
+p.drawTo(dx: 80, dy: -200)
 
-// Draw a vertical line, up and to the left
-p.drawTo(dx: -25, dy: 50)
+//outside lines
+goToOrigin()
+p.goto(dx: -100, dy: -100)
+p.drawTo(dx: 0, dy: 200)
+p.goto(dx: 200, dy: 0)
+p.drawTo(dx: 0, dy: -200)
 
-// Go back to origin
-p.goToOrigin()
+//draw inside lines
+goToOrigin()
+p.goto(dx: 40, dy: 100)
+p.drawTo(dx: 0, dy: -200)
+p.goto(dx: -80, dy: 0)
+p.drawTo(dx: 0, dy: 200)
+goToOrigin()
+p.goto(dx: 40, dy: 60)
+p.drawTo(dx: -80, dy: 0)
+p.goto(dx: 0, dy: -120)
+p.drawTo(dx: 80, dy: 0)
+goToOrigin()
+p.goto(dx: -40, dy: -100)
+p.drawTo(dx: 40, dy: 40)
+p.drawTo(dx: 40, dy: -40)
+p.goto(dx: 0, dy: 200)
+p.drawTo(dx: -40, dy: -40)
+p.drawTo(dx: -40, dy: 40)
+goToOrigin()
+p.goto(dx: -60, dy: -35)
+p.drawTo(dx: 0, dy: 70)
+p.goto(dx: -20, dy: 10)
+p.drawTo(dx: 0, dy: -90)
+goToOrigin()
+p.goto(dx: 60, dy: 35)
+p.drawTo(dx: 0, dy: -70)
+p.goto(dx: 20, dy: -10)
+p.drawTo(dx: 0, dy: 90)
 
-// Change the pen color
-p.penColor = .red
+//draw lines on four corner
+goToOrigin()
+p.goto(dx: -100, dy: 100)
+p.drawTo(dx: 20, dy: 0)
+p.drawTo(dx: 40, dy: -40)
+goToOrigin()
+p.goto(dx: 100, dy: 100)
+p.drawTo(dx: -20, dy: 0)
+p.drawTo(dx: -40, dy: -40)
+goToOrigin()
+p.goto(dx: -100, dy: -100)
+p.drawTo(dx: 20, dy: 0)
+p.drawTo(dx: 40, dy: 40)
+goToOrigin()
+p.goto(dx: 100, dy: -100)
+p.drawTo(dx: -20, dy: 0)
+p.drawTo(dx: -40, dy: 40)
+//shape outside
+goToOrigin()
+p.goto(dx: -180, dy: 20)
+    func shape1() {
+        p.turn(degrees: 30)
+        p.addArc(radius: 35, angle: 120)
+        p.drawTo(dx: 50, dy: -31)
+        p.drawTo(dx: -50, dy: -31)
+}
+shape1()
+goToOrigin()
+p.turn(degrees: 210)
+p.goto(dx: -180, dy: -80)
+shape1()
 
-// Draw a curve, down and to the right
-p.addArc(radius: 50, angle: -45)
+goToOrigin()
+p.goto(dx: 140, dy: 100)
 
-/*:
- ## Show the Live View
- Don't see any results?
- 
- Remember to show the Live View (1 then 2):
- 
- ![timeline](timeline.png "Timeline")
+func shape2(){
+    p.turn(degrees: 60)
+    p.addArc(radius: 45, angle: 120)
+    p.drawTo(dx: 19, dy: 38)
+    p.drawTo(dx: -18, dy: 38)
 
- ## Use source control
- To keep your work organized, receive feedback, and earn a high grade in this course, regular use of source control is a must.
- 
- Please commit and push your work often.
- 
- ![source_control](source-control.png "Source Control")
- */
+}
+shape2()
+goToOrigin()
+p.goto(dx: 140, dy: -20)
+p.turn(degrees: 180)
+shape2()
