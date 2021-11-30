5150b789965ace6feb9ff21d8012f900099db919
Justin Fu <JFu@Lcs.on.ca>
2021-11-29 20:11:43 -0500

    almost finished poster 2

--- Playground.playground/Pages/poster 2.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/poster 2.xcplaygroundpage/Contents.swift
@@ -16,6 +16,8 @@ let preferredHeight = 600
 import Cocoa
 import PlaygroundSupport
 import CanvasGraphics
+import CoreFoundation
+import Darwin
 
 // Create canvas
 let canvas = Canvas(width: preferredWidth, height: preferredHeight)
@@ -52,6 +54,10 @@ canvas.drawAxes(withScale: true, by: 50, color: .black)
  [Documentation](http://russellgordon.ca/CanvasGraphics/Documentation/) is available.
 
  */
+// all colors
+let green = Color(hue: 99, saturation: 54, brightness: 72, alpha: 100)
+let grey = Color(hue: 84, saturation: 4, brightness: 88, alpha: 100)
+
 
 //draw background from 0,0 - 400-400
 canvas.lineColor = .black
@@ -60,25 +66,38 @@ for background in stride(from: 0, through: 400, by: 1){
     canvas.drawLine(from: Point(x: background, y: 0), to: Point(x: background, y: 400))
 }
 canvas.highPerformance = false
-//circles
-canvas.fillColor = .green
+
+//white circles
+canvas.fillColor = grey
 for i in stride(from: 0, through: 400, by: 40){
     for u in stride(from: 0, through: 400, by: 40){
-        canvas.drawEllipse(at: Point(x: i, y: u), width: 35, height: 35)
-    }
-                            
+    if i + u <= 400{
+        canvas.drawEllipse(at: Point(x: u, y: i), width: 35, height: 35)
+    }}
 }
 
+
+//circles
+canvas.fillColor = green
+for i in stride(from: 0, through: 400, by: 40){
+    for u in stride(from: 0, through: 400, by: 40){
+        if i + u > 400 || u == 0 || i == 0{
+        canvas.drawEllipse(at: Point(x: i, y: u), width: 35, height: 35)
+        }}
+                            
+}
+canvas.highPerformance = true
 //background 2
-canvas.lineColor = .green
+canvas.lineColor = green
 for bg in stride(from: 0, through: 400, by: 1){
     canvas.drawLine(from: Point(x: bg, y: 400), to: Point(x: bg, y: 600))
 }
+canvas.highPerformance = false
 
-//white circles
-canvas.fillColor = .white
-
-
+canvas.drawText(message: "pixies", at: Point(x: 20, y: 420), size: 65, kerning: 0)
+canvas.drawText(message: "9 pm over 21", at: Point(x: 20, y: 515), size: 10, kerning: 0)
+canvas.drawText(message: "december 13 1986", at: Point(x: 20, y: 530), size: 10, kerning: 0)
+canvas.drawText(message: "saturday", at: Point(x: 20, y: 545), size: 10, kerning: 0)
 /*:
  ## Show the Live View
  Don't see any results?
