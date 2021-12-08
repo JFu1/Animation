ce66943339e8ee66e0d06a114d8762d9efcf0a97
Justin Fu <JFu@Lcs.on.ca>
2021-12-08 12:09:33 -0500

    figuring out one solution to my ideal poster

--- Playground.playground/Pages/poster 5 final most important.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/poster 5 final most important.xcplaygroundpage/Contents.swift
@@ -1,6 +1,6 @@
 //: [Previous](@previous) / [Next](@next)
 /*:
-## Canvas size
+ ## Canvas size
  
  Set the size of your desired canvas by adjusting the constants on lines 7 and 8.
  */
@@ -42,20 +42,82 @@ PlaygroundPage.current.liveView = canvas
 
 // Show a grid
 canvas.drawAxes(withScale: true, by: 50, color: .black)
-
+canvas.lineColor = .black
 /*:
  ## Add your code
  
  Beginning on line 61, you can add your own code.
-  
+ 
  [Documentation](http://russellgordon.ca/CanvasGraphics/Documentation/) is available.
-
+ 
  */
-for y in stride(from: 575, through: 50, by: -100){
+//all spine main bone.
+for y in stride(from: 570, through: 150, by: -100){
     for x in stride(from: 50, through: 350, by: 100){
-        canvas.drawLine(from: Point(x: x, y: y ), to: Point(x: x, y:y-50 ))
+        
+        canvas.drawLine(from: Point(x: x, y: y), to: Point(x: x, y: y-50), lineWidth: 13, capStyle: .round, dashed: false)
     }
 }
+
+//left side spine bone
+for i in stride(from: 35, through: 375, by: 100){
+    canvas.drawLine(from: Point(x: i, y: 555), to: Point(x: i+15, y: 565), lineWidth: 5, capStyle: .round, dashed: false)
+    canvas.drawLine(from: Point(x: i, y: 545), to: Point(x: i+15, y: 555), lineWidth: 5, capStyle: .round, dashed: false)
+    canvas.drawLine(from: Point(x: i, y: 535), to: Point(x: i+15, y: 545), lineWidth: 5, capStyle: .round, dashed: false)
+    canvas.drawLine(from: Point(x: i, y: 525), to: Point(x: i+15, y: 535), lineWidth: 5, capStyle: .round, dashed: false)
+}
+
+
+
+//right side spine bone.
+for u in stride(from: 50, through: 375, by: 100){
+    canvas.drawLine(from: Point(x: u, y: 565), to: Point(x: u+15, y: 555), lineWidth: 5, capStyle: .round, dashed: false)
+    canvas.drawLine(from: Point(x: u, y: 555), to: Point(x: u+15, y: 545), lineWidth: 5, capStyle: .round, dashed: false)
+    canvas.drawLine(from: Point(x: u, y: 545), to: Point(x: u+15, y: 535), lineWidth: 5, capStyle: .round, dashed: false)
+    canvas.drawLine(from: Point(x: u, y: 535), to: Point(x: u+15, y: 525), lineWidth: 5, capStyle: .round, dashed: false)
+    
+}
+
+for p in stride(from: 460, through: 150, by: -100){
+    for t in stride(from: 30, through: 375, by: 100){
+        if p>400{
+            
+            canvas.drawLine(from: Point(x: t, y: p), to: Point(x: t+40, y: p), lineWidth: 10, capStyle: .round, dashed: false)
+            canvas.drawLine(from: Point(x: t, y: p-15), to: Point(x: t+40, y: p-15), lineWidth: 10, capStyle: .round, dashed: false)
+            canvas.drawLine(from: Point(x: t, y: p-30), to: Point(x: t+40, y: p-30), lineWidth: 10, capStyle: .round, dashed: false)
+            
+        } else {
+            canvas.drawLine(from: Point(x: t, y: p-5), to: Point(x: t+40, y: p-5), lineWidth: 15, capStyle: .round, dashed: false)
+        }
+        
+        
+    
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//background
 canvas.drawRectangle(at: Point(x: 0, y: 200), width: 400, height: 200)
 
 /*:
@@ -65,7 +127,7 @@ canvas.drawRectangle(at: Point(x: 0, y: 200), width: 400, height: 200)
  Remember to show the Live View (1 then 2):
  
  ![timeline](timeline.png "Timeline")
-
+ 
  ## Use source control
  To keep your work organized, receive feedback, and earn a high grade in this course, regular use of source control is a must.
  
